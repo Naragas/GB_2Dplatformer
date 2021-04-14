f83123a30cd7f33c357acd41edfe3d48a0806454
Naragas <Naragas@gmail.com>
2021-04-15 02:20:44 +0300

    HomeWork1

---
+++ Assets/Scripts/CharacterView.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class CharacterView: MonoBehaviour
+{
+    public SpriteRenderer SpriteRenderer;
+    //add other useful refs
+}
+
